99ba1a460000415441dc12822840150bbf8d7e46
jjambread <jjambrad@naver.com>
2022-11-24 12:23:17 +0900

    저장1

---
+++ java_project/src/ch03_test_1018/Hello.java
@@ -0,0 +1,19 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실행은 ctrl + f11
+		// 주석이 보통은 ctrl + / : 단일형 주석
+		// 한번 더 ctrl + / : 취소가 됨
+
+		// 여러 행 주석 : ctrl + shift + /
+		// 여러 행 주석 해체 : ctrl + shift + \ ... 포멧팅이 깨질 수 있음
+		// 자동 포멧 정렬 : ctrl + shift + f
+		System.out.println("Hello 이상용 자바");
+		System.out.println("Hello 이상용 자바");
+		System.out.println("Hello 이상용 자바");
+	}
+
+}
