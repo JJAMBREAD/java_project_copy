3a71ddb1a8ecf274af98887b36150bbf5174ca7b
jjambread <jjambrad@naver.com>
2022-12-02 15:38:06 +0900

    1202 6교시 저장2

--- java_project/src/ch05_test2_1202/PersonTest.java
+++ java_project/src/ch05_test2_1202/PersonTest.java
@@ -5,15 +5,18 @@ public class PersonTest {
 	 public static void main(String[] args) {
 		 //기본 생성자 호출
 		 Person jmj = new Person();
+		 //데이터에 직접 접근 할거냐?
 	//	  jmj.name = "주민정";
 		  jmj.weight = 100;
 		  jmj.height = 170;
+		  
+		  //set(메서드)를 이용해서 name 값 설정하기.
 		  jmj.setName("주민정세터로추가했음.");
 		  jmj.showInfo();
 		  System.out.println("get메서드로 이름호출"
 				  +jmj.getName());
 		  
-		  //set(메서드)를 이용해서 name 값 설정하기.
+		
 
 		  
 		  // 매개변수가 1개인 생성자를 호출.
@@ -23,7 +26,7 @@ public class PersonTest {
 		  jmj2.showInfo();
 		  
 		  // 매개변수가 3개인 생성자를 호출.
-		  Person jmj3 = new Person("주민정", 100, 170);
+		  Person jmj3 = new Person("주민정", 170, 100);
 		  jmj3.showInfo();
 //		  personKim.name = "������";
 //		  personKim.weight = 85.5F;
