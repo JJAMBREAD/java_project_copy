7af119b90832ba20e15fb41886542ae495f19fad
jjambread <jjambrad@naver.com>
2022-12-05 13:30:58 +0900

    221205 점심시간 저장

--- java_project/src/ch06_cooperation_1019/Bus.java
+++ java_project/src/ch06_cooperation_1019/Bus.java
@@ -5,9 +5,9 @@ public class Bus {
 		 int passengerCount;        // 승객 수
 		 int money;                 // 버스의 수입
 		    
-		 public Bus(int busNumebr)   // 버스 번호를 매개변수로 받는 생성자
+		 public Bus(int busNumber)   // 버스 번호를 매개변수로 받는 생성자
 		 {   
-			  this.busNumber = busNumebr;
+			  this.busNumber = busNumber;
 		 }
 		
 		 public void take(int money)   // 승객이 낸 돈을 받음
--- java_project/src/ch06_sample_1019/CallAnotherConst.java
+++ java_project/src/ch06_sample_1019/CallAnotherConst.java
@@ -1,5 +1,8 @@
 package ch06_sample_1019;
 class Person{
+
+
+
 	String name;
 	int age;
 	
@@ -12,11 +15,20 @@ class Person{
 		this.age = age;
 	}
 	
+	//정체가 메서드임. 그런데, 반환하는 형: 클래스 형 (Person)
 	Person returnItSelf() {
-		return this;
+		return this; // 클래스 형이 (Person) 인 참조형 변수를 가리킨다. : this 가.
+	}
+	
+	//get 메서드임. 반환하는 형 : 문자열 형(String)
+	public String getName() {
+		return name;
+	}
+	//get 메서드임. 반환하는 형 : 기본 형(정수형 int)
+	public int getAge() {
+		return age;
 	}
 }
-
 public class CallAnotherConst {
 
 	public static void main(String[] args) {
@@ -25,8 +37,32 @@ public class CallAnotherConst {
 		System.out.println(noName.name);
 		System.out.println(noName.age);
 		
-		//Person p = noName.returnItSelf();
-		//System.out.println(p);
-		//System.out.println(noName);
+		System.out.println(noName.getName());
+		System.out.println(noName.getAge());
+		
+		// 변수를 선언 하는데 형. 참조형(Person)
+		// p : 참조형 변수
+		// returnItSelf() 메서드를 호출하면 결과 값의 타입은(형) : Person 형입니다.
+		// 그래서, 받을 변수의 타입 또한 일치 시켜서 Person으로 선언해서 받습니다.
+		Person p = noName.returnItSelf();
+		//String p2 = noName.returnItSelf(); // 타입 미스매치로 에러남.
+
+		// 만약, 해당 형이 아닌 다른 형으로 받는 방법은 없을까?
+		// 있음. 가장 큰 형인 최상위 클래스라고함 . Object
+		// Object로 받을 수 이쏙, 원래의 형으로 되돌릴수도 있습니다.
+		// 형변을 통해서.
+		
+		Object p3 = noName.returnItSelf(); // Object 는 그릇이 가장 큼.
+		Person p4 = (Person)p3;
+		
+		
+		// ctrl 누른 상태에서 , 해당 메서드 명을 클릭하면, 메서드가 정의한 곳으로 간다.
+		System.out.println("p 출력 ");
+		System.out.println(p);
+		System.out.println("p3 출력 ");
+		System.out.println(p3);
+		System.out.println("p4 출력 ");
+		System.out.println(p4);
+		System.out.println(noName);
 	}
 }
