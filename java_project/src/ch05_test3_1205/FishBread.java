8dad1c166dca80c2a762a410c2c55333f77593d1
jjambread <jjambrad@naver.com>
2022-12-05 10:12:34 +0900

    221205 2교시 저장1

--- java_project/src/ch05_test3_1205/FishBread.java
+++ java_project/src/ch05_test3_1205/FishBread.java
@@ -10,9 +10,30 @@ public class FishBread {
 	// 예2) getPowder() 메서드로 접근.
 	// 자동으로 해당 멤버들의 대해서 set/get 자동으로 만들었음.
 		
+	// 기본 생성자 -> JVM이 자동으로 만들어줌. 조건) 해당 생성자가 하나도 없을 경우.
+	// 오버로드 -> 매개 변수가 2개인 생성자를 만들기.
+	
+	//기본생성자 -> 추가로 직접 만듦.
+	public FishBread() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// 매개변수가 2개인 생성자 -> 자동으로 만듦.
+	public FishBread(String powder, String redBean) {
+		//상속이라는 개념을 통해서 부모 클래스, 상위 클래스를 호출 할 때 사용하는 키워드.
+		//초기화하는 역할.
+		// 암암리에 Object 최고상위 클래스 상속을 받아둠.
+		super();
+		this.powder = powder;
+		this.redBean = redBean;
+	}
+	
+
+
 	public String getPowder() {
 		return powder;
 	}
+
 	public void setPowder(String powder) {
 		this.powder = powder;
 	}
